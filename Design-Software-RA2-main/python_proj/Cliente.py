37100c07836d321c77f61e07b7cf9bb0678872e7
Felipe Sphair <107360437+felipesphair@users.noreply.github.com>
2023-10-06 16:54:57 -0300

    Delete Design-Software-RA2-main directory

--- Design-Software-RA2-main/python_proj/Cliente.py
+++
@@ -1,7 +0,0 @@
-class Cliente:
-    def __init__(self, id, nome, cpf, telefone, cep):
-        self.id = id
-        self.nome = nome
-        self.cpf = cpf
-        self.telefone = telefone
-        self.cep = cep
